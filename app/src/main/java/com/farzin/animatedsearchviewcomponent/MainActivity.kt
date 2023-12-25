4b62fcf71a0ec5b1e4353d4b750d4e7c1ceee25f
farzinzeynal <ferzo.zeynal@mail.ru>
2023-12-26 00:31:37 +0400

    animated search view modifications

--- app/src/main/java/com/farzin/animatedsearchviewcomponent/MainActivity.kt
+++ app/src/main/java/com/farzin/animatedsearchviewcomponent/MainActivity.kt
@@ -20,83 +20,10 @@ import com.farzin.animatedsearchviewcomponent.components.AnimatedSearchView
 
 class MainActivity : AppCompatActivity() {
 
-
-    lateinit var animatedSearchView: AnimatedSearchView
-    lateinit var button: Button
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        animatedSearchView = findViewById(R.id.animatedSearchView)
-        button = findViewById(R.id.button)
-
-        button.setOnClickListener {
-            expandSearchView()
-        }
-
-
-
-    }
-
-    private fun expandSearchView() {
-        val targetWidth = getScreenWidth() // Set target width to match_parent
-
-        // Create a ValueAnimator for width animation
-        val valueAnimator = ValueAnimator.ofInt(animatedSearchView.width, targetWidth)
-        valueAnimator.addUpdateListener { animation ->
-            val animatedValue = animation.animatedValue as Int
-            val layoutParams = animatedSearchView.layoutParams
-            layoutParams.width = animatedValue
-            animatedSearchView.layoutParams = layoutParams
-        }
-
-        // Set animation duration
-        valueAnimator.duration = 700 // Adjust duration as needed
-
-        // Start the animation
-        valueAnimator.start()
-
-        valueAnimator.doOnEnd {
-            showKeyboard(animatedSearchView)
-        }
-    }
-
-    private fun collapsSearchView() {
-        val targetWidth = 50 // Set target width to match_parent
-
-        // Create a ValueAnimator for width animation
-        val valueAnimator = ValueAnimator.ofInt(animatedSearchView.width, targetWidth)
-        valueAnimator.addUpdateListener { animation ->
-            val animatedValue = animation.animatedValue as Int
-            val layoutParams = animatedSearchView.layoutParams
-            layoutParams.width = animatedValue
-            animatedSearchView.layoutParams = layoutParams
-        }
-
-        // Set animation duration
-        valueAnimator.duration = 700 // Adjust duration as needed
-
-        // Start the animation
-        valueAnimator.start()
-
-        valueAnimator.doOnEnd {
-
-        }
-    }
-
-    private fun dpToPx(dp: Int): Int {
-        val density = resources.displayMetrics.density
-        return (dp * density).toInt()
-    }
-
-    private fun getScreenWidth(): Int {
-        return resources.displayMetrics.widthPixels - 100
-    }
-
-    private fun showKeyboard(view: View) {
-        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT)
     }
 
 
--- app/src/main/java/com/farzin/animatedsearchviewcomponent/components/AnimatedSearchView.kt
+++ app/src/main/java/com/farzin/animatedsearchviewcomponent/components/AnimatedSearchView.kt
@@ -1,14 +1,22 @@
 package com.farzin.animatedsearchviewcomponent.components
 
+import android.animation.ValueAnimator
+import android.annotation.SuppressLint
 import android.content.Context
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.LinearLayout
+import androidx.core.animation.doOnEnd
 import androidx.core.view.isVisible
 import com.farzin.animatedsearchviewcomponent.databinding.LayoutAnimatedSearchBinding
+import com.google.android.material.internal.ViewUtils.hideKeyboard
+import com.google.android.material.internal.ViewUtils.showKeyboard
+import kotlin.math.roundToInt
 
 class AnimatedSearchView @JvmOverloads constructor(
     ctx: Context,
@@ -22,11 +30,15 @@ class AnimatedSearchView @JvmOverloads constructor(
     init {
         binding.imgClose.setOnClickListener {
             binding.edtSearch.setText("")
+            collapsSearchView()
+        }
+
+        binding.imgSearchButton.setOnClickListener {
+            expandSearchView()
         }
 
         binding.edtSearch.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
@@ -34,10 +46,69 @@ class AnimatedSearchView @JvmOverloads constructor(
             }
 
             override fun afterTextChanged(s: Editable?) {
-
             }
         })
     }
+
+
+    @SuppressLint("RestrictedApi")
+    private fun expandSearchView() {
+        binding.imgSearchButton.isVisible=false
+        binding.searchLayout.isVisible = true
+
+        val targetWidth = getScreenWidth()
+
+        val valueAnimator = ValueAnimator.ofInt(binding.searchLayout.width, targetWidth)
+        valueAnimator.addUpdateListener { animation ->
+            val animatedValue = animation.animatedValue as Int
+            val layoutParams = binding.searchLayout.layoutParams
+            layoutParams.width = animatedValue
+            binding.searchLayout.layoutParams = layoutParams
+        }
+
+        valueAnimator.duration = 300
+
+        valueAnimator.start()
+
+        valueAnimator.doOnEnd {
+            binding.edtSearch.requestFocus()
+            showKeyboard(binding.root)
+        }
+    }
+
+    @SuppressLint("RestrictedApi")
+    private fun collapsSearchView() {
+        binding.imgSearchButton.isVisible=true
+        binding.searchLayout.isVisible = false
+
+        val targetWidth = 50
+
+        val valueAnimator = ValueAnimator.ofInt(binding.searchLayout.width, targetWidth)
+        valueAnimator.addUpdateListener { animation ->
+            val animatedValue = animation.animatedValue as Int
+            val layoutParams = binding.searchLayout.layoutParams
+            layoutParams.width = animatedValue
+            binding.searchLayout.layoutParams = layoutParams
+        }
+
+        valueAnimator.duration = 300
+
+        valueAnimator.start()
+
+        valueAnimator.doOnEnd {
+            hideKeyboard(binding.root)
+        }
+    }
+
+    private fun dpToPx(dp: Int): Int {
+        val density = resources.displayMetrics.density
+        return (dp * density).toInt()
+    }
+
+    private fun getScreenWidth(): Int {
+        return resources.displayMetrics.widthPixels.toDouble().times( 0.85).roundToInt()
+    }
+
 
     fun requestInput(view: (EditText) -> Unit) {
         view(binding.edtSearch)
